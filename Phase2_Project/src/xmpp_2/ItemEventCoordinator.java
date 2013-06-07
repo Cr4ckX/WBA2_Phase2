2b53c850f9e30024c060ac3dc75e93a04b55fadd
David Petersen <crackxswag@gmail.com>
2013-06-07 16:37:48 +0200

    Möglichkeit um auf die konkreten ItemInhalte zugreifen zu können.

--- Phase2_Project/src/xmpp_2/ItemEventCoordinator.java
+++ Phase2_Project/src/xmpp_2/ItemEventCoordinator.java
@@ -9,7 +9,7 @@ public class ItemEventCoordinator<T> implements ItemEventListener {
     public void handlePublishedItems(ItemPublishEvent items)
     {
         System.out.println("Item count: " + items.getItems().size());
-        System.out.println(items);
+        System.out.println(items.getItems().get(0));
 	}
 
 }
